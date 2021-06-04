90a1f0c3f423bd3e95d8fc51da2aef5492c4eb4a
everaldoxd <evecontas@gmail.com>
2021-06-04 07:15:45 -0300

    SiteCriptoApp atualizado

---
+++ src/Topo.js
@@ -0,0 +1,22 @@
+//IMPORTAR MODULO
+import React from 'react';
+import logo from './imagens/logo.png'
+
+//class
+
+export default class Topo extends React.Component{
+  render(){
+    return(
+      
+      <div>
+
+      </div>
+
+
+
+
+    )
+  }
+}
+
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Bitcoin from './Bitcoin';
+import React from 'react'
+
+
+
+//IMPORTAR COMPONENTES
+import Principal from './Principal';
+import Topo from './Topo';
+
+import './Estilos.css';
+
+ReactDOM.render(
+<div>
+  <Topo />
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' exact={true} component={Principal} />
+      <Route path='/bitcoin' exact={true} component={Bitcoin} />
+    </Switch>
+  </BrowserRouter>
+</div>
+,document.getElementById('root'));
